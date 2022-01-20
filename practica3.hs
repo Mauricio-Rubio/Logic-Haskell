06e8faeba7e135d0ee002d39e55bd8daf8da5dfd
Mauricio-Rubio <mau.r.h54@gmail.com>
2022-01-20 11:02:40 -0600

    Primero commit

---
+++ practica3.hs
@@ -0,0 +1,115 @@
+data Var = A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z deriving (Show, Eq, Ord) 
+
+data Formula = Prop Var
+    |Neg Formula
+    |Formula :&: Formula
+    |Formula :|: Formula
+    |Formula :=>: Formula
+    |Formula :<=>: Formula deriving (Show, Eq, Ord)
+infix 9 :&:
+infix 9 :|:
+infix 7 :=>:
+infix 8 :<=>:
+-------------------------------------------------------------
+-- Ejercicio 1.-Negación
+negar :: Formula -> Formula
+negar (Neg f) = f
+negar (Prop v) = Neg (Prop v)
+negar (p :&: q) = (negar p) :|: (negar q)
+negar (p :|: q) = (negar p) :&: (negar q) 
+negar (p :=>: q) = (p) :&: (negar q) 
+negar (p :<=>: q) = negar ((p :=>: q) :&: (q :=>: p))
+-------------------------------------------------------------
+
+-------------------------------------------------------------
+-- Ejercicio 2.-Variables de la Fórmula
+variables :: Formula -> [Var]
+variables (Prop a) = [a]
+variables (p :&: q) = (variables p) ++ (variables q)
+variables (p :|: q) = (variables p) ++ (variables q)
+variables (p :=>: q) = (variables p) ++ (variables q)
+variables (p :<=>: q) = (variables p) ++ (variables q)
+-------------------------------------------------------------
+
+-------------------------------------------------------------
+equivalencia :: Formula -> Formula
+equivalencia (Neg p) = negar p 
+equivalencia (p :=>: q) = (negar (equivalencia p)) 
+    :|: (equivalencia q)
+
+
+interpretacionVariable :: Var -> [(Var,Bool)] -> Bool
+interpretacionVariable v ((x,b) : xs) = if v == 
+    x then b else interpretacionVariable v xs
+
+negacion :: Bool -> Bool 
+negacion True = False
+negacion False = True 
+
+conjuncion :: Bool -> Bool -> Bool 
+conjuncion True True = True
+conjuncion x y = False
+
+disyuncion :: Bool -> Bool -> Bool 
+disyuncion False False = False
+disyuncion x y = True
+
+implicacion :: Bool -> Bool -> Bool 
+implicacion True False = False
+implicacion x y = True
+
+dimplicacion :: Bool -> Bool -> Bool 
+dimplicacion True True = True
+dimplicacion x y = False
+
+interpretacion :: Formula -> [(Var,Bool)] -> Bool
+interpretacion (Prop v) xs = interpretacionVariable v xs
+interpretacion (Neg f) xs = negacion (interpretacion f xs)
+interpretacion (p :&: q) xs = conjuncion (interpretacion p xs) (interpretacion q xs)
+interpretacion (p :|: q) xs = disyuncion (interpretacion p xs) (interpretacion q xs)
+interpretacion (p :=>: q) xs = implicacion (interpretacion p xs) (interpretacion q xs)
+interpretacion (p :=>: q) xs = dimplicacion (interpretacion p xs) (interpretacion q xs)
+--interpretacion
+
+--Funcion para concatenar un valor de tipo A a una lista de tipo A
+--agregarValor :: a -> [[a]] -> [[a]]
+--agregarValor x [] = []
+--agregarValor x (y:ys) = ((x:y):agregarValor x ys)
+--agregarValor x (y:ys) = ((x:y):agregarValor x ys)
+
+agregarValor :: a -> [[a]] -> [[a]]
+agregarValor x [] = []
+agregarValor x (y:ys) = ((x:y):agregarValor x ys)
+
+tablaVerdad :: [Var] -> [[(Var,Bool)]]
+tablaVerdad [x] = [[(x,True)],[(x,False )]]
+tablaVerdad (x:xs) = (agregarValor (x,True) (tablaVerdad xs)) ++ (agregarValor (x,False) (tablaVerdad xs))
+
+conjuncionLista1 :: [Bool] -> Bool
+conjuncionLista1 [b] = b
+conjuncionLista1 (x:xs) = conjuncion x (conjuncionLista1 xs)
+
+conjuncionLista2 :: [Bool] -> Bool
+conjuncionLista2 [b] = b
+conjuncionLista2 (x:xs) = if x then conjuncionLista2 xs else x
+
+contiene ::Eq a => a -> [a] -> Bool
+contiene x [] = False
+contiene x (y:ys) = if x == y then True else contiene x ys
+
+conjunto :: Eq a => [a] -> [a]
+conjunto [] = []
+conjunto (x:xs) = if contiene x xs then conjunto xs else (x:conjunto xs)
+
+--variables :: Formula -> [Var]
+--variables f = []
+
+--interpretacion :: Formula -> [(Var,Bool)] -> Bool
+--interpretacion f t = True
+
+interpretacionLista :: Formula -> [[(Var,Bool)]] -> [Bool]
+interpretacionLista f [] = []
+interpretacionLista f (x:xs) = (interpretacion f x : interpretacionLista f xs)
+
+tautologia :: Formula -> Bool
+tautologia f = conjuncionLista1 (interpretacionLista f (tablaVerdad (variables f)))
